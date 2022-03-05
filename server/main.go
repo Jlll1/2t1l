b59373715a0f0826a628cf11d932b62bfb72693e
Jlll1 <arghantentua@tutanota.com>
2022-03-05 22:14:18 +0100

    Enable CORS

--- server/main.go
+++ server/main.go
@@ -6,6 +6,21 @@ import (
 	"net/http"
 )
 
+func setHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, cache-control")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func createRoom(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("hello")
 }
@@ -13,10 +28,13 @@ func createRoom(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := "4444"
 
-	http.HandleFunc("/createroom", createRoom)
+	r := http.NewServeMux()
+	r.HandleFunc("/createroom", createRoom)
+
+	m := setHeaders(r)
 
 	log.Print("Server starting at localhost:" + port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, m); err != nil {
 		log.Fatal(err)
 	}
 }

a0334795daa9d58f0a75b95f076fafc2f7da5c6c
Jlll1 <arghantentua@tutanota.com>
2022-03-18 15:02:34 +0100

    Add unit testing framework

---
+++ client/test/TestRunner.ts
@@ -0,0 +1,38 @@
+import { MessageChannel } from "worker_threads";
+import { ExampleTest } from "./example.test";
+
+export class Test {
+    public static case(name: string, test: Function): void {
+        try {
+            console.log(`=== Running ${name}...`);
+            test();
+        } catch(err) {
+            if (err instanceof AssertionError) {
+                console.log(
+                    `=== Test Failed! = ${name} =\n` +
+                    `= ${err.message}\n` +
+                    `===`
+                    );
+            } else {
+                throw err;
+            }
+        }
+    }
+}
+
+export class AssertionError {
+    constructor(public message: string){}
+}
+
+export class Assert {
+    public static equal(actual: any, expected: any): void {
+        if (expected === actual) {
+            return;
+        }
+
+        throw new AssertionError(`Expected value to be <<${expected}>>, but got <<${actual}>>.`)
+    }
+}
+
+/* Register Test Classes */
+new ExampleTest();
